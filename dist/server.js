a855c96e2f3eea59fe3fa72f5aa20dc12ab4a0ff
Mussa Charles <mussacharles60@gmail.com>
2022-02-22 23:58:08 +0300

    update references

---
+++ dist/server.js
@@ -0,0 +1,30 @@
+"use strict";
+// reference: https://developer.okta.com/blog/2018/11/15/node-express-typescript
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000; // default port to listen
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer, {});
+io.on("connection", (socket) => {
+    // ...
+});
+// Configure Express to use EJS
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    // render the index template
+    res.render("index");
+});
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server running on port ${port}.`);
+});
+//# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,35 @@
 
 // reference: https://developer.okta.com/blog/2018/11/15/node-express-typescript
 
-import express, { Request, Response, NextFunction } from 'express';
-import bodyParser from 'body-parser';
-
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import path from "path";
+
 const app = express();
 const port: any = process.env.PORT || 3000; // default port to listen
 
-// Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    // render the index template
-    res.render( "index" );
-} );
-
-app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-  console.log(`Server running on port ${port}.`);
 });
 
+io.on("connection", (socket) => {
+  // ...
+});
+
+
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  // render the index template
+  res.render("index");
+});
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server running on port ${port}.`);
+});
