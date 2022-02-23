0452771397e2ae70cc3f6aca3ee3f45c27a7eeb4
Mussa Charles <mussacharles60@gmail.com>
2022-02-23 03:34:47 +0300

    make it better

--- tools/copyAssets.ts
+++ tools/copyAssets.ts
@@ -1,4 +1,13 @@
 import * as shell from "shelljs";
 
 // Copy all the view templates
-shell.cp( "-R", "src/views", "dist/" );
+shell.cp( "-R", ["src/views"], "dist/" );
+shell.cp( "-R", ["src/public/css"], "dist/public/" );
+
+
+// // unused lib
+// npm install copyfiles --save-dev
+// npm script
+// "scripts": {
+//     "html": "copyfiles -u 1 app/**/*.html app/**/*.css dist/"
+//   }
